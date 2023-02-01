2ef752e6fb6f3422c9e4f22849fe8e95434f507d
Anjiang-Wei <1020681930@qq.com>
2023-02-01 00:39:05 -0800

    hope it works

--- tool/writer.py
+++ tool/writer.py
@@ -5,6 +5,7 @@ import re
 # 1e00000000000000 -> SYSTEM_MEM
 charmap = {}
 
+placement = {} # taskname --> (proc, mem)
 
 def read_from_file(filename):
     with open(filename, "r") as fin:
@@ -33,35 +34,75 @@ def init(lines):
             break
 
 def map_task(lines):
-    res = {} # taskname --> (proc, mem)
     taskname = ""
     proc = ""
     mem = ""
     skip = False
+    enter_critical = False
     for line in lines:
         if "MAP_REPLICATE_TASK" in line or "SELECT_TASK_SOURCES" in line:
             # "TARGET PROCS" also appears in map_replicate_task
             # "memory=..." also appears in select_task_sources
             skip = True
+            taskname = ""
+            proc = ""
+            mem = ""
         if "MAP_TASK for" in line:
-            skip = False
-            # MAP_TASK for CorrectGhostOperators(index_point=(2,0,0))<5536>
-            line = line.replace("MAP_TASK for ", "")
-            if "(index_point" in line:
-                taskname = line.split("(")[0]
+            if enter_critical:
+                # potential conflict, skip this
+                skip = True
+                taskname = ""
+                proc = ""
+                mem = ""
             else:
-                taskname = line.split("<")[0]
+                skip = False
+                enter_critical = True
+                # print("enter_critical = True", line)
+                # MAP_TASK for CorrectGhostOperators(index_point=(2,0,0))<5536>
+                line = line.replace("MAP_TASK for ", "")
+                if "(index_point" in line:
+                    taskname = line.split("(")[0]
+                else:
+                    taskname = line.split("<")[0]
         # TARGET PROCS: 1d0000000000000f
         if "TARGET PROCS:" in line and skip == False:
             proc_char = line.replace("TARGET PROCS: ", "")
             proc = charmap[proc_char]
+            placement[taskname] = (proc, "UNKNOWN")
         # PhysicalInstance[4000000001000003](memory=1e00000000000004,domain=<125 ...
         if "PhysicalInstance" and "memory=" in line and skip == False:
             mem_char = re.search(r"memory=(.*),domain", line).group(1)
             mem = charmap[mem_char]
-            res[taskname] = (proc, mem)
-    print(res)
+            placement[taskname] = (proc, mem)
+            enter_critical = False
+        if "OUTPUT INSTANCES" in line:
+            enter_critical = False
+    # print(placement)
 
+def infer_one_kind(proc_str, default_memory_lst):
+    conversion = {"TOC_PROC": "GPU",
+                  "OMP_PROC": "OMP",
+                  "LOC_PROC": "CPU",
+                  "IO_PROC" : "IO"}
+    task_list = []
+    region_list = []
+    for task in placement.keys():
+        proc, mem = placement[task]
+        if proc == proc_str and mem in default_memory_lst:
+            task_list.append(task)
+        if proc == proc_str and mem not in default_memory_lst:
+            region_list.append((task, conversion[proc], conversion[mem]))
+    task_str = ",".join(task_list)
+    if len(task_list) > 0:
+        print(f"Task {task_str} {conversion[proc_str]}")
+    for region in region_list:
+        print(f"Region {region[0]} {region[1]} {region[2]}")
+
+def infer():
+    infer_one_kind("TOC_PROC", ["GPU_FB_MEM", "UNKNOWN"])
+    infer_one_kind("OMP_PROC", ["SYSTEM_MEM", "UNKNOWN"])
+    infer_one_kind("LOC_PROC", ["SYSTEM_MEM", "UNKNOWN"])
+    infer_one_kind("IO_PROC", ["UNKNOWN"])
 
 
 if __name__ == "__main__":
@@ -69,4 +110,4 @@ if __name__ == "__main__":
     lines = trim(read_from_file(filename))
     init(lines)
     map_task(lines)
-    # print('\n'.join(lines[:20]))
+    infer()
